80ea22bba2e02fe3013e457ef8cb2f815c6a87d1
Sobana Qadus <sobana.qadus@booking.com>
2025-09-03 15:23:31 +0100

    added some code and api key.

--- js/movies.js
+++ js/movies.js
@@ -4,12 +4,20 @@ export default class MovieHelper {
         // Define our API root URL, we can then add specific paths onto the end for different queries
         this.api_root = "https://api.themoviedb.org/3"
         // Define our API key here
-        this.api_key = "YOUR_API_KEY"
+        this.api_key = "68ad4349da5eeb9915c6b7077a42ec7d"
     }
 
     // Use this API endpoint: https://developer.themoviedb.org/reference/discover-movie
     async getMovies() {
-        // return ...
+        try {
+            const response = await fetch(this.api_root+"/discover/movie")
+            const data = await response.json();
+            console.log (response)
+        } catch (error) {
+            
+        }
+
     }
+    
 
 }
